87d137e3807a5763bb065dfc97f7ddc676c28df8
Stradalec <pochtatipo6@gmail.com>
2024-11-10 08:09:43 +0700

    Added start files to task 2

--- app/src/main/java/com/example/labandroid6/MainActivity.kt
+++ app/src/main/java/com/example/labandroid6/MainActivity.kt
@@ -1,16 +1,105 @@
 package com.example.labandroid6
 
+import android.R.attr.data
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import timber.log.Timber
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var  adapter: MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
 
+        adapter = MyAdapter(emptyList()){ url ->
+            val clipboard = this.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("copied", url)
+            clipboard.setPrimaryClip(clip)
+            Timber.i(url)
+        }
+
+        val recyclerView: RecyclerView = findViewById(R.id.rView)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = adapter
+        CoroutineScope(Dispatchers.IO).launch {
+            val list = ParsePhotos("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
+            withContext(Dispatchers.Main){
+                displayImageList(list)
+            }
+        }
+
+
+
+
+
+
+    }
+
+
+    private fun ParsePhotos(inputUrl: String): List<String> {
+        val client = OkHttpClient()
+        var photoLinks: List<String> = listOf("1")
+        val request = Request.Builder()
+            .url(inputUrl)
+            .build()
+        client.newCall(request).execute().body()?.string()
+        val response = client.newCall(request).execute()
+        val body = response.body()?.string()
+        if (body != null) {
+            val gson = GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+
+            val wrapper = gson.fromJson(body, com.example.labandroid6.Wrapper::class.java)
+
+            photoLinks = wrapper.photos.photo.map { photo ->
+                "https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_z.jpg"
+            }
+            Timber.i("Copied link: $photoLinks")
+
+            wrapper.photos.photo.forEachIndexed { index, photo ->
+                if (index % 5 == 0) {
+                    Timber.d("ID: ${photo.id}, Owner: ${photo.owner}, Secret = ${photo.secret}, Server = ${photo.server}, Farm = ${photo.farm}, Title: ${photo.title}, IsPublic = ${photo.isItPublic}, IsFriend = ${photo.isFriend},IsFamily= ${photo.isFamily}")
+                }
+            }
+
+        }
+        return photoLinks
+    }
+
+    private fun displayImageList( imageUrlList: List<String>) {
+        val recyclerView: RecyclerView = findViewById(R.id.rView)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = MyAdapter(imageUrlList) { url ->
+            val clipboard = this.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("copied", url)
+            clipboard.setPrimaryClip(clip)
+            Timber.i(url)
+        }
     }
 }
