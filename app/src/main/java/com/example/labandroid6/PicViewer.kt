0b4d4923b805f835be8b1e609447246448c14b65
Stradalec <pochtatipo6@gmail.com>
2024-11-10 10:01:34 +0700

    Something is done

--- app/src/main/java/com/example/labandroid6/PicViewer.kt
+++ app/src/main/java/com/example/labandroid6/PicViewer.kt
@@ -1,9 +1,13 @@
 package com.example.labandroid6
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
@@ -15,11 +19,28 @@ class PicViewer : AppCompatActivity() {
         setContentView(R.layout.pic_viewer)
         val imageView: ImageView =  findViewById(R.id.picView)
         val picLink = intent.getStringExtra("linkToPicture")
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
 
 
         Glide.with(this)
             .load(picLink)
             .into(imageView);
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item_favorite -> {
+                // реализация порядка действий для выбранного элемента
+                Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
